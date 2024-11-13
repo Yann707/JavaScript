4300826a75c88ca3dc30b89fc600884a6fb7354f
Yann707 <zongyanw@metropolia.fi>
2024-11-13 13:40:29 +0200

    yan

--- Module2/Assignment1/Module2-1.js
+++ Module2/Assignment1/Module2-1.js
@@ -1,2 +1,9 @@
-console.log("I'm printing to console!");
-
+const numbers = [];
+        for (let i = 0; i < 5; i++) {
+            let num = prompt(`Enter a number :`);
+            numbers.push(Number(num));
+        }
+        console.log("Numbers in reverse order:");
+        for (let i = numbers.length - 1; i >= 0; i--) {
+            console.log(numbers[i]);
+        }
