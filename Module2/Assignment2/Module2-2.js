9f83c262f856e37558e4469ef9c42618edbffeaf
Yann707 <zongyanw@metropolia.fi>
2024-11-13 14:13:48 +0200

    yan

--- Module2/Assignment2/Module2-2.js
+++ Module2/Assignment2/Module2-2.js
@@ -1,2 +1,17 @@
-let userName = prompt("Enter your name:");
-document.write(`Hello, ${userName}!`);
+window.onload = function listParticipants() {
+    let participantCount = parseInt(prompt("Enter the number of participants:"));
+    let participants = [];
+    for (let i = 0; i < participantCount; i++) {
+        participants.push(prompt("Enter the name of participant " + (i + 1) + ":"));
+    }
+
+    participants.sort();
+
+    let listHtml = "<ol>";
+    for (let i = 0; i < participants.length; i++) {
+        listHtml += "<li>" + participants[i] + "</li>";
+    }
+    listHtml += "</ol>";
+
+    document.body.innerHTML += listHtml;
+}
